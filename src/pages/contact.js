47603bb6f2eb937b4edb21ffd17984aa96cc62cd
Tomasz Karny <karny.tomasz@gmail.com>
2020-06-01 23:08:46 +0200

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Contact } from 'components/Contact/Contact'
+
+import { Layout } from 'components/Layout/Layout'
+
+export default () => (
+  <Layout>
+    <Contact />
+  </Layout>
+)
