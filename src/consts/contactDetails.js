fdf143343bbbb79f7b3e174c005ad9219837a671
Tomasz Karny <karny.tomasz@gmail.com>
2020-06-01 23:04:39 +0200

    added contact details

---
+++ src/consts/contactDetails.js
@@ -0,0 +1,5 @@
+export const EMAIL_ADDRESS = 'tadeusz_karny@wp.pl'
+export const ADDRESS = 'Hryniewicze 31, 15 - 378 Białystok'
+export const PHONE_NUMBER = '+48 604 253 145'
+export const FACEBOOK_URL = 'www.facebook.com/kowalstwo.karny'
+export const GOOGLE_MAP_DIRECTIONS = { lat: 53.400344, lng: 22.805769 }
