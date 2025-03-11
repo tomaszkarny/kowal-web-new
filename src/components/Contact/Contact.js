996fccc5cba39030103b14493b89c8eb830c634c
Tomasz Karny <tomaszkarny@MacBook-Pro-Tomasz.local>
2025-03-11 23:59:31 +0100

    Remove unused StyledMapElement import from Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,10 +4,7 @@ import { ContactDetails } from 'components/Contact/ContactDetails/ContactDetails
 import { ContactForm } from 'components/Contact/ContactForm/ContactForm'
 import { WrappedGoogleMap } from 'components/Contact/GoogleMap/GoogleMap'
 
-import {
-  ContainerWrapper,
-  StyledMapElement,
-} from 'components/Contact/GoogleMap/GoogleMap.styles'
+import { ContainerWrapper } from 'components/Contact/GoogleMap/GoogleMap.styles'
 import { ContactWrapper } from 'components/Contact/Contact.styles'
 
 export const Contact = () => {
@@ -17,13 +14,9 @@ export const Contact = () => {
         <ContactDetails />
         <ContactForm />
       </ContactWrapper>
-      <WrappedGoogleMap
-        isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places${process.env.GATSBY_GOOGLE_API_KEY ? `&key=${process.env.GATSBY_GOOGLE_API_KEY}` : ''}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<ContainerWrapper />}
-        mapElement={<StyledMapElement />}
-      />
+      <ContainerWrapper>
+        <WrappedGoogleMap isMarkerShown />
+      </ContainerWrapper>
     </>
   )
 }
