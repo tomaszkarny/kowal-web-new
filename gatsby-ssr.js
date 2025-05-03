06309b317cd2b89224738cf0ea216a93d3e90744
Your Name <karny.tomasz@gmail.com>
2025-05-04 00:25:53 +0200

    Add Ahrefs analytics with environment variable support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -9,3 +9,20 @@ const I18NextWrapper = require('./src/components/i18n/I18NextWrapper').default;
 exports.wrapRootElement = ({ element }) => {
   return React.createElement(I18NextWrapper, null, element);
 };
+
+/**
+ * Add Ahrefs analytics script to the document head
+ * Only adds the script if the environment variable is defined
+ */
+exports.onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.GATSBY_AHREFS_ANALYTICS_KEY) {
+    setHeadComponents([
+      React.createElement('script', {
+        key: 'ahrefs-analytics',
+        src: 'https://analytics.ahrefs.com/analytics.js',
+        'data-key': process.env.GATSBY_AHREFS_ANALYTICS_KEY,
+        async: true
+      })
+    ]);
+  }
+};
